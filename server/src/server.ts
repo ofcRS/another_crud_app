9b553b9c2cc1713dfb824366d34d4f5402d50c2a
a <azeric054@gmaill.com>
2019-12-09 23:23:09 +0400

    install typeorm

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,6 +4,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
+import 'reflect-metadata';
+import { createConnection } from 'typeorm';
 
 import logger from 'services/logger';
 
@@ -15,27 +17,40 @@ dotenv.config({
 
 const PORT = process.env.PORT;
 
-const app = express();
 
-app.use(bodyParser.json());
+        const connection = createConnection({
+            type: 'mysql',
+            host: 'localhost',
+            port: 3306,
+            username: 'root',
+            password: '1234',
+            database: 'node_learning',
+            entities: [],
+            synchronize: true,
+            logging: false,
+        });
 
-app.use(cookieParser());
+        const app = express();
 
-app.use((req, res, next) => {
-    res.set({
-        'Access-Control-Allow-Origin': 'http://localhost:3000',
-        'Access-Control-Allow-Methods': '*, DELETE',
-        'Access-Control-Allow-Headers':
-            'Origin, X-Requested-With, Content-Type, Accept',
-        'Access-Control-Allow-Credentials': true,
-    });
-    next();
-});
+        app.use(bodyParser.json());
 
-app.use(routes);
+        app.use(cookieParser());
 
-const server = http.createServer(app);
+        app.use((req, res, next) => {
+            res.set({
+                'Access-Control-Allow-Origin': 'http://localhost:3000',
+                'Access-Control-Allow-Methods': '*, DELETE',
+                'Access-Control-Allow-Headers':
+                    'Origin, X-Requested-With, Content-Type, Accept',
+                'Access-Control-Allow-Credentials': true,
+            });
+            next();
+        });
 
-server.listen(PORT, () => {
-    logger.info(`server start on ${PORT}`);
-});
+        app.use(routes);
+
+        const server = http.createServer(app);
+
+        server.listen(PORT, () => {
+            logger.info(`server start on ${PORT}`);
+        });
